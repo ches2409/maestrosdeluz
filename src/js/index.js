88516565b3921eaf6c1dd63f16167de39e3232d5
Andrés Gómez <andresg2409@gmail.com>
2019-04-03 10:17:44 -0500

    actualizacin de slid, modificacion de responsive 1a parte

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import navigationOffCanvas from './components/navigation_off_canvas'
 import transparentHeader from './components/transparent_header'
 import slid from './components/slid'
+// import wow from '../../node_modules/wowjs/dist/wow.min.js'
 
+// new wow().init()
 navigationOffCanvas()
 transparentHeader()
 slid()
