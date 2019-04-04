07b3bcd8b9c41128aa46a0b526b972239297b735
Andrés Gómez <andresg2409@gmail.com>
2019-04-04 02:29:00 -0500

    incorporado animaciones de wowjs

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import navigationOffCanvas from './components/navigation_off_canvas'
 import transparentHeader from './components/transparent_header'
 import slid from './components/slid'
-// import wow from '../../node_modules/wowjs/dist/wow.min.js'
 
-// new wow().init()
+
 navigationOffCanvas()
 transparentHeader()
 slid()
+
+
